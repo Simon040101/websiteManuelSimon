e1b751722c0729ad080f81d4c0f71da1df5a4d52
Manuel R <reismann.m@gmx.at>
2019-03-25 18:18:00 +0100

    Merging

--- Project_XOVO/XOVO/Models/FeedItem.cs
+++ Project_XOVO/XOVO/Models/FeedItem.cs
@@ -8,13 +8,34 @@ namespace XOVO.Models
     public class FeedItem
     {
         // Variablen and Konstanten
+        public int Id { get; set; }
 
         // properties
-        public  User User { get; set; }
+        public  User UserForFeed { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public string ImgPath { get; set; }
+        public string FeedContent { get; set; }
 
         // ctors
+        public FeedItem() : this(0, null, DateTime.MinValue, "", ""){ }
 
-
+        public FeedItem(int feedId, User userForFeed, DateTime creationDateTime, string imgPath, string feedCont)
+        {
+            this.Id = feedId;
+            this.UserForFeed = userForFeed;
+            this.CreationDateTime = creationDateTime;
+            this.ImgPath = imgPath;
+            this.FeedContent = feedCont;
+        }
+        public FeedItem(int feedId, int userIdForFeed, DateTime creationDateTime, string imgPath, string feedCont)
+        {
+            this.Id = feedId;
+            this.UserForFeed = new User();
+            this.UserForFeed.ID = userIdForFeed;
+            this.CreationDateTime = creationDateTime;
+            this.ImgPath = imgPath;
+            this.FeedContent = feedCont;
+        }
 
 
     }
--- Project_XOVO/XOVO/Models/sql/web.sql
+++ Project_XOVO/XOVO/Models/sql/web.sql
@@ -1,7 +1,7 @@
-create database XOVO collate utf8_general_ci;
+use xovo;
+select * from users;
 
-use XOVO;
-
+drop table users;
 create table users(
 
 	id int not null auto_increment,
@@ -12,18 +12,30 @@ create table users(
     username varchar(100) null, 
     email varchar(100) not null unique, 
     passwrd varchar(100) not null,
-    isAdmin tinyint not null default 0
-
+    isAdmin tinyint not null default 0,
+    layout_color varchar(100),
+    background_login varchar(100),
+    
     constraint id_PK primary key(id)
 
 
 
 )engine = InnoDB;
-
-insert into users values (1, "Simon", "Raass", null, null, "simon", "simon@gmail.com", "123456", 0);
-insert into users values (2, "Manuel", "Reismann", null, null, "manuel", "manuel@gmx.at", "hallo123", 0);
-insert into users values (3, "admin", "admin", null, null, "admin", "admin@swp.at", sha1("Admin1!"), 0);
-
+insert into users values (1, "Simon", "Raass", "2001-04-01" , 0, "Simon", "Simon@swp.at", sha2("123456789", 256), 0, "blue", "/Content/img/background_login_registration.jpg");
+insert into users values (2, "Manuel", "Reismann", "2000-01-31" , 0, "Manuel", "Manuel@swp.at", sha2("123456789", 256), 1, "red", "/Content/img/background_login_registration.jpg");
 select * from users;
 
-alter table users auto_increment = 1;
+create table Feed(
+feed_id int not null auto_increment,
+user_id int not null,
+creationDateTime datetime,
+imagePath varchar(300),
+content text,
+
+constraint feed_id_PK primary key(feed_id),
+constraint user_id_FK foreign key(user_id) references users(id)
+)engine = InnoDB;
+
+insert into feed values(null, 1, "2019-03-12 09:11:00", "/Content/img/background_login_registration.jpg", "erster Feed");
+insert into feed values(NULL, 1, "2019-03-13 17:01:00", "/Content/img/background_login_registration.jpg", "zweiter Feed");
+insert into feed values(null, 1, "2019-03-14 12:38:00", "/Content/img/background_login_registration.jpg", "dritter Feed");
